94ede440821faf249467a5b5dde876a6563f6c9c
jno <glegarto@gbox.ncf.edu.ph>
2025-03-24 18:02:06 +0800

    edit the user and schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    fullName: { type: String, required: true },
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  },
-  { timestamps: true }
-);
+const UserSchema = new mongoose.Schema({
+  fullName: String,
+  username: String,
+  email: String,
+  password: String, // Will not be selected in find() for security reasons
+  age: Number,
+  course: String,
+  dateOfBirth: Date,
+  joinedAt: { type: Date, default: Date.now },
+  gender: String
+});
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
